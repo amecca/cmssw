b879c6caae502f39166c61470f8c765dbf815bb8
Alberto <alberto.mecca@cern.ch>
2023-07-19 12:59:16 +0200

    Improvements to submit scripts

--- RecoMuon/MuonSeedGenerator/scripts/haddChunks.sh
+++ RecoMuon/MuonSeedGenerator/scripts/haddChunks.sh
@@ -4,19 +4,18 @@
 
 # Set paths
 inputDir=${1:-.}
-echo "INFO: input chunks searched in" $inputDir
+echo "INFO: input chunks searched in" $inputDir "($(realpath $inputDir))"
 realpath $inputDir | grep -q AAAOK || { echo 'ERROR: you are probably trying to merge unfinished jobs' 1>&2 ; exit 1 ; }
 inputFiles=$(ls $inputDir/Chunk_*/seedRebuild.root)
 echo "INFO: found" $(echo $inputFiles | wc -w) "file(s)"
-inputFiles=$(echo $inputFiles | sed "s/^/file:/g" | tr "\n" "," | sed "s/,$//" ; echo)
+inputFiles=$(echo $inputFiles | tr -d '\n' | tr " " "\n" | sed "s/^/file:/g" | tr "\n" "," ; echo)
 outputFile=$inputDir/seedRebuild.root
 echo "INFO: output will be stored in" $outputFile
 
-# Launch the job in background
-{
-    edmCopyPickMerge outputFile=$outputFile inputFiles=$inputFiles 2>&1 | gzip --fast > edmCopyPickMerge.log.gz
-    for i in $(seq 3) ; do  # Ring the bell three times
-	printf "\a"
-	sleep 0.4
-    done
-} &
+set -x
+edmCopyPickMerge outputFile=$outputFile inputFiles="$inputFiles" 2>&1 | gzip --fast > edmCopyPickMerge.log.gz
+set +x
+for i in $(seq 3) ; do  # Ring the bell three times
+    printf "\a"
+    sleep 0.4
+done
