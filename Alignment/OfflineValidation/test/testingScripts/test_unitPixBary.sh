26b15da96918754a5436ed27814d65e7d4882eca
Alberto Mecca <alberto.mecca@cern.ch>
2024-09-23 16:18:03 +0200

    fixup! Add unit tests for PixBary validation

--- Alignment/OfflineValidation/test/testingScripts/test_unitPixBary.sh
+++ Alignment/OfflineValidation/test/testingScripts/test_unitPixBary.sh
@@ -4,20 +4,14 @@ function die { echo $1: status $2 ; exit $2; }
 
 runrange=376370-379254
 
-thistest="Alignment/PixelBarycentre single yaml configuration unitTest (test 1/3)"
+thistest="Alignment/PixelBarycentre single yaml configuration unitTest (test 1/2)"
 echo "TESTING $thistest..."
 pushd test_yaml/PixBary/single/testSinglePixBary/unitTest/$runrange
 ./cmsRun validation_cfg.py config=validation.json || die "Failure running $thistest" $?
 popd
 
-thistest="Alignment/PixelBarycentre single yaml configuration mp3619 (test 2/3)"
+thistest="Alignment/PixelBarycentre single yaml configuration mp3619 (test 2/2)"
 echo "TESTING $thistest..."
 pushd test_yaml/PixBary/single/testSinglePixBary/mp3619/$runrange
-./cmsRun validation_cfg.py config=validation.json || die "Failure running $thistest" $?
-popd
-
-thistest="Alignment/PixelBarycentre single json configuration unitTest (test 3/3)"
-echo "TESTING $thistest..."
-pushd test_json/PixBary/single/testSinglePixBary/unitTest/$runrange
 ./cmsRun validation_cfg.py config=validation.json || die "Failure running $thistest" $?
 popd
