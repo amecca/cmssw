7328424f6c3dfb0e80dd2888ded0a5ae784de30b
Alberto Mecca <alberto.mecca@cern.ch>
2024-09-23 16:16:36 +0200

    fixup! New script to table results from PixelBaryCentreAnalyzer

--- Alignment/OfflineValidation/scripts/extractBaryCentre.py
+++ Alignment/OfflineValidation/scripts/extractBaryCentre.py
@@ -13,12 +13,15 @@ class TFileContext(object):
         self.tfile.Close()
 
 def display_results(t_data, style='twiki'):
+    before = after = None
     if  (style == 'twiki'):
         header_fmt = ' | '.join( ['{:^6s}'] + ['{:^9s}' ] * (len(t_data.keys())-1) )
         row_fmt    = ' | '.join( ['{:6d}' ] + ['{:9.6f}'] * (len(t_data.keys())-1) )
         header_fmt = '| '+header_fmt+' |'
         row_fmt    = '| '+row_fmt   +' |'
     elif(style == 'latex'):
+        before     = '\\begin{tabular}{' + 'c'*len(t_data.keys()) + '}'
+        after      = '\\end{tabular}'
         header_fmt = ' & '.join( ['{:^6s}'] + ['{:^9s}' ] * (len(t_data.keys())-1) )  + ' \\\\\n\\hline'
         row_fmt    = ' & '.join( ['{:6d}' ] + ['{:9.6f}'] * (len(t_data.keys())-1) ) + ' \\\\'
     elif(style == 'csv'):
@@ -27,21 +30,23 @@ def display_results(t_data, style='twiki'):
     else:
         raise RuntimeError('Unknown style "%s" for table'%(style))
 
+    if(before is not None): print(before)
     print( header_fmt.format(*t_data.keys()) )
     for i, run in enumerate(t_data['run']):
         print(row_fmt.format(run, *[t_data[k][i] for k in t_data.keys() if not k == 'run']))
+    if(after is not None): print(after)
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('fname'            , metavar='FILE')
     parser.add_argument('-p', '--partition', default='BPIX', help='Tracker partition (e.g. BPIX, FPIX, BPIXLYR1, etc.). Default: %(default)s')
-    parser.add_argument('-l', '--list'         , action='store_true', dest='list_entries', help='List the contents of file and exit')
+    parser.add_argument('-l', '--list-content' , action='store_true', dest='list_content', help='List the contents of file and exit')
     parser.add_argument(      '--list-branches', action='store_true', help='List the branches of the tree and exit')
     parser.add_argument('-t', '--type'     , default='barycentre', choices=('barycentre', 'beamspot'), type=str.lower, help='Default: %(default)s')
-    parser.add_argument(      '--label'    , default=None, help='Additional label that is appended to the folder name')
+    parser.add_argument(      '--label'    , default=None, help='Additional label that is appended to the folder name (i.e. PixelBaryCentreAnalyzer by default)')
     parser.add_argument(      '--quality'  , action='store_true', help='Read results with the WithPixelQuality flag (default: %(default)s)')
-    parser.add_argument('-s', '--style'    , default='twiki', choices=('twiki', 'latex', 'csv'), type=str.lower, help='Table style for the output (choices: %(choices)s)')
+    parser.add_argument('-s', '--style'    , default='twiki', choices=('twiki', 'latex', 'csv'), type=str.lower, help='Table style for the output (default: %(default)s)')
     parser.add_argument(      '--loglevel' , metavar='LEVEL', default='WARNING', help='Level for the python logging module. Can be either a mnemonic string like DEBUG, INFO or WARNING or an integer (lower means more verbose).')
 
     args = parser.parse_args()
@@ -49,9 +54,6 @@ def main():
     logging.basicConfig(format='%(levelname)s:%(module)s:%(funcName)s: %(message)s', level=loglevel)
     logging.debug('args: %s', args)
 
-    if(args.list_entries):
-        list_entries(args.fname)
-        return 0
 
     folder    = 'PixelBaryCentreAnalyzer' if not args.quality          else 'PixelBaryCentreAnalyzerWithPixelQuality'
     tree_name = 'PixelBarycentre'         if args.type == 'barycentre' else 'BeamSpot'
@@ -60,6 +62,10 @@ def main():
     columns = ['run'] + [(args.partition if args.type=='barycentre' else 'BS')+'.'+coord for coord in ('x', 'y', 'z')]
 
     with TFileContext(args.fname) as tfile:
+        if(args.list_content):
+            list_content(tfile)
+            return 0
+
         tfolder = tfile.Get(folder)
         if(not tfolder):
             raise KeyError('Folder "%s" not found in "%s"' %(folder, args.fname))
@@ -68,12 +74,13 @@ def main():
         tree = tfolder.Get(tree_name)
         if(not tree):
             logging.error('Tree "%s" not found; content of file "%s":', tree_name, args.fname)
-            list_entries(args.fname)
+            list_content(args.fname)
             raise KeyError(tree_name)
 
         if(args.list_branches):
-            print('Branches (%s/%s): %s' % (folder, tree_name, [b.GetName() for b in tree.GetListOfBranches()]))
+            list_branches(tree, folder=folder)
             return 0
+
         rdf = ROOT.RDataFrame(tree)
         logging.info('Reading "%s"', tree_name)
         results = rdf.AsNumpy(columns)
@@ -82,13 +89,19 @@ def main():
 
     return 0
 
-def list_entries(fname):
-    with TFileContext(fname) as tfile:
-        for tfolder in tfile.GetListOfKeys():
-            print(tfolder.GetName())
-            for key in tfolder.ReadObj().GetListOfKeys():
-                tree = key.ReadObj()
-                print('\t%s %-16s: %d branches, %d entries' %(tree.ClassName(), key.GetName(), len(tree.GetListOfBranches()), tree.GetEntries()))
+
+def list_content(tfile):
+    for tfolder in tfile.GetListOfKeys():
+        for key in tfolder.ReadObj().GetListOfKeys():
+            obj = key.ReadObj()
+            print('%s\t%s/%s\t%d branches, %d entries' %(obj.ClassName(), tfolder.GetName(), key.GetName(), len(obj.GetListOfBranches()), obj.GetEntries()))
+
+
+def list_branches(tree, folder_name=''):
+    branches = [b.GetName() for b in tree.GetListOfBranches()]
+    logging.info('Branches (%s/%s): %d', (folder_name, tree.GetName(), len(branches)))
+    print('\n'.join(branches))
+
 
 if __name__ == '__main__':
     exit(main())
